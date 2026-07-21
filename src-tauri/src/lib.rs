d507c493eb9860ad765e97ac8954d32a289f835d
William Johnson <wnj@tutamail.com>
2026-07-21 09:11:05 -0400

    Version update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,7 +8,7 @@ const APP_NAME: &str = "Genesis";
 #[cfg(not(feature = "genesis"))]
 const APP_NAME: &str = "Kinesis";
 
-const VERSION: &str = "0.3.0";
+const VERSION: &str = "0.3.1";
 
 fn get_window_title() -> String {
     format!("{} v{}", APP_NAME, VERSION)
